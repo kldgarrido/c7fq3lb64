789cce443367245354b2f4e1910f90cf3278d462
kld <kldgarrido.com>
2017-06-08 14:23:33 -0500

    upgrade

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,37 +1,13 @@
 class ExpensesController < ApplicationController
+  
   def index
-
-  	concept = params[:concept]
-    category = params[:category_id]
-
-  	if !concept.blank? and !category.blank? 
-      concept = concept.downcase
-  		@expenses = Expense.where('concept LIKE ? AND category_id=?', '%'+concept+'%', category)
-
-  	elsif !concept.blank? and category.blank?
-      concept = concept.downcase 
-  		@expenses = Expense.where('concept LIKE ?', '%'+concept+'%')
-
-    elsif concept.blank? and !category.blank?
-      @expenses = Expense.where(category: category)
-
-    else
-      @expenses = Expense.order("date DESC")
-  		
-  	end
-
-  	#concept = params[:concept].downcase
-  	#category = params[:category_id].downcase
-
-    #@expenses = Expense.order("date DESC")
-     
-     #@expenses = Expense.where(nil)
-
-     
-     #@expenses = @expenses.category(params[:category_id]) if params[:category_id].present?
-     #@expenses = @expenses.concept(params[:concept]) if params[:concept].present?
-
-
+    @expenses = Expense.order("date DESC")
+    if params[:concept].present?
+      @expenses = @expenses.where("concept LIKE ?", "%#{params[:concept]}%")
+    end
+    if params[:category_id].present?
+      @expenses = @expenses.where("category_id = ?", params[:category_id])
+    end
   end
 
 end
