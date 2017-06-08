d5c57a5b5dac91ad44682e64233fb46fad35b950
kld <kldgarrido.com>
2017-06-08 14:03:01 -0500

    mission complete

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,37 @@
 class ExpensesController < ApplicationController
   def index
-     @expenses = Expense.order("date DESC")
+
+  	concept = params[:concept]
+    category = params[:category_id]
+
+  	if !concept.blank? and !category.blank? 
+      concept = concept.downcase
+  		@expenses = Expense.where('concept LIKE ? AND category_id=?', '%'+concept+'%', category)
+
+  	elsif !concept.blank? and category.blank?
+      concept = concept.downcase 
+  		@expenses = Expense.where('concept LIKE ?', '%'+concept+'%')
+
+    elsif concept.blank? and !category.blank?
+      @expenses = Expense.where(category: category)
+
+    else
+      @expenses = Expense.order("date DESC")
+  		
+  	end
+
+  	#concept = params[:concept].downcase
+  	#category = params[:category_id].downcase
+
+    #@expenses = Expense.order("date DESC")
+     
+     #@expenses = Expense.where(nil)
+
+     
+     #@expenses = @expenses.category(params[:category_id]) if params[:category_id].present?
+     #@expenses = @expenses.concept(params[:concept]) if params[:concept].present?
+
+
   end
+
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: expenses
+#
+#  id          :integer          not null, primary key
+#  category_id :integer
+#  date        :date
+#  concept     :string
+#  amount      :decimal(, )
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Expense < ActiveRecord::Base
   belongs_to :category
+
+  scope :category, -> (category) {where category: category}
+  scope :concept, -> (concept) {where concept: concept}
 end
