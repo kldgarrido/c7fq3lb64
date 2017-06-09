a9ed2e58ce6dbd3db00d01a1ba5d8201e3dcc916
kld <kldgarrido.com>
2017-06-08 22:25:29 -0500

    add has_many :expense to user model 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-	has_many :expense         
+  has_many :expenses
+
 end
